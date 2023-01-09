31297f7506a39fcc800327424e35a578fa50fc43
habiibullah <ajiboyhabiib@gmail.com>
2023-01-09 18:55:13 +0100

    fix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -33,4 +33,4 @@ class Square(Rectangle):
     def __str__(self):
         """prints [Rectangle] <width>/<height>"""
         return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
-                                         self.__size, self.__size
+         self.__size, self.__size
