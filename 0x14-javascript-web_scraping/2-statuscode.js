3a6b1e24f3f5b52b089522ec41ab2a82c1a6ad09
habiibullah <ajiboyhabiib@gmail.com>
2023-04-26 06:50:24 +0000

    Write a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,5 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code:' + response.statusCode);
-  }
+  console.log('code: ' + response.statusCode);
 });
